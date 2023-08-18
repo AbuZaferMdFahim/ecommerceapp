2d5aa6a6ff5897d343f6924dfa0e1c18c6bb5836
AbuZaferMdFahim <mightyanim007@gmail.com>
2023-08-18 21:25:55 +0600

    Password and updateaddre

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from . import views
 from django.contrib.auth import views as auth_view
 
-from . forms import LoginForm,MyPasswordResetForm,MyPasswordChangeForm
+from . forms import LoginForm,MyPasswordResetForm,MyPasswordChangeForm,MyPasswordForm
 
 from . forms import LoginForm,MyPasswordResetForm
 
@@ -26,8 +26,18 @@ urlpatterns = [
     #login authentication
     path('registration/', views.CustomerRegistrationView.as_view(),name="CustomerRegistration"),
     path('accounts/login/', auth_view.LoginView.as_view(template_name= 'app/login.html', authentication_form = LoginForm), name='login'),
-    path('password-reset/', auth_view.PasswordResetView.as_view(template_name= 'app/password_reset.html', form_class = MyPasswordResetForm), name='password_reset'),
+    
     path('passwordChange/', auth_view.PasswordChangeView.as_view(template_name='app/passwordChange.html', form_class= MyPasswordChangeForm, success_url='/passwordChangeDone'),name='passwordChange'),
     path('passwordChangeDone/', auth_view.PasswordChangeDoneView.as_view(template_name='app/passwordChangeDone.html'),name='passwordChangeDone'),
+    path('logout/', auth_view.LogoutView.as_view(next_page = 'login'), name='logout'),
+
+    #password reset
+    path('passwordReset/',auth_view.PasswordResetView.as_view(template_name = 'app/passwordReset.html',form_class= MyPasswordResetForm),name='passwordReset'),
+
+    path('passwordReset/done/',auth_view.PasswordResetDoneView.as_view(template_name = 'app/passwordResetDone.html'),name='passwordResetDone'),
+
+    path('passwordResetConfrim/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name = 'app/passwordResetConfirm.html',form_class= MyPasswordForm),name='passwordResetConfirm'),
+
+    path('passwordResetComplete/',auth_view.PasswordResetCompleteView.as_view(template_name = 'app/passwordResetComplete.html'),name='passwordResetComplete'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
